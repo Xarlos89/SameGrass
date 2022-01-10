9255066855485c753adc95e144cc555f2ea0ab21
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2022-01-10 09:30:05 +0100

    Update Functions.py

--- SameGrass/Functions.py
+++ SameGrass/Functions.py
@@ -70,6 +70,9 @@ def Search_State_MinMax(StateName,MinPop,MaxPop):
 	except:
 		print("No city containing \"{}\" was found with a population between {} and {}.".format(place,MinPop,MaxPop))
 
+		
+		
+# Useless due to Google's terrible pricing model on things that should be free.
 ################ GOOGLE PLACES FUNCTIONS ################
 # https://developers.google.com/maps/documentation/places/web-service/search-nearby#maps_http_places_nearbysearch-txt 
 
@@ -113,49 +116,49 @@ def Search_State_MinMax(StateName,MinPop,MaxPop):
 # 		print("No airport within {} miles of {}".format(miles,CityReturned))
 # 		has_Airport_near = False
 
-def Find_all_services_between_pops(MinPop,MaxPop,distance):
-	list_to_search = []	# Make a list of palces to search.
-	for row in range(0,len(df)): # Search ENTIRE excel doc.
-		city = df.iloc[row]['City']
-		state = df.iloc[row]['State']
-		currentPop = df.iloc[row][2019]
-		if int(MinPop) <= currentPop <= int(MaxPop): # if the current pop is between min and max...
-			new_place = [] # Set up data for google API
-			new_place.append(city)
-			new_place.append(state)
-			new_place.append(currentPop)
+# def Find_all_services_between_pops(MinPop,MaxPop,distance):
+# 	list_to_search = []	# Make a list of palces to search.
+# 	for row in range(0,len(df)): # Search ENTIRE excel doc.
+# 		city = df.iloc[row]['City']
+# 		state = df.iloc[row]['State']
+# 		currentPop = df.iloc[row][2019]
+# 		if int(MinPop) <= currentPop <= int(MaxPop): # if the current pop is between min and max...
+# 			new_place = [] # Set up data for google API
+# 			new_place.append(city)
+# 			new_place.append(state)
+# 			new_place.append(currentPop)
 			
-			list_to_search.append(new_place) # Send a list into a list. 
+# 			list_to_search.append(new_place) # Send a list into a list. 
 
-	meters = int(distance)*1609.344 # Quickmaths
-	dist_in_miles = meters/1609.344 # Quickmaths
-	for i in enumerate(list_to_search):
-		# Get latitude and Longitude
-		url_LatLong = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query={}&key={}'.format(i[1][0].replace(" ", "%"),apikey)
-		response = requests.request("GET", url_LatLong).json()
-		latitude = ((response['results'])[0])['geometry']['location']['lat']
-		longitude = ((response['results'])[0])['geometry']['location']['lng']
+# 	meters = int(distance)*1609.344 # Quickmaths
+# 	dist_in_miles = meters/1609.344 # Quickmaths
+# 	for i in enumerate(list_to_search):
+# 		# Get latitude and Longitude
+# 		url_LatLong = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query={}&key={}'.format(i[1][0].replace(" ", "%"),apikey)
+# 		response = requests.request("GET", url_LatLong).json()
+# 		latitude = ((response['results'])[0])['geometry']['location']['lat']
+# 		longitude = ((response['results'])[0])['geometry']['location']['lng']
 
-		# Search lat and long for hospitals, airports... within the radius.
-		url_hospital = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=Hospital&location={}%2C{}&radius={}&type=HOSPITAL&key={}'.format(str(latitude),str(longitude),str(meters),apikey)
-		url_airport = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=Airport&location={}%2C{}&radius={}&type=AIRPORT&key={}'.format(str(latitude),str(longitude),str(meters),apikey)
-		response_hospital = requests.request("GET", url_hospital).json()
-		response_airport = requests.request("GET", url_airport).json()
+# 		# Search lat and long for hospitals, airports... within the radius.
+# 		url_hospital = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=Hospital&location={}%2C{}&radius={}&type=HOSPITAL&key={}'.format(str(latitude),str(longitude),str(meters),apikey)
+# 		url_airport = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=Airport&location={}%2C{}&radius={}&type=AIRPORT&key={}'.format(str(latitude),str(longitude),str(meters),apikey)
+# 		response_hospital = requests.request("GET", url_hospital).json()
+# 		response_airport = requests.request("GET", url_airport).json()
 
-		# If the hospitals, airports are found, say they are...
-		if len(response_hospital['results']) >= 1:
-			has_Hospital_near = 'Yes'
-		else:
-			has_Hospital_near = 'No'
+# 		# If the hospitals, airports are found, say they are...
+# 		if len(response_hospital['results']) >= 1:
+# 			has_Hospital_near = 'Yes'
+# 		else:
+# 			has_Hospital_near = 'No'
 
-		if len(response_airport['results']) >= 1:
-			has_airport_near = 'Yes'
-		else:
-			has_airport_near = 'No'
+# 		if len(response_airport['results']) >= 1:
+# 			has_airport_near = 'Yes'
+# 		else:
+# 			has_airport_near = 'No'
 		
-		# Make it pretty and print that shit. 
-		print("City: {}\nState: {}\nPopulation: {}\n\nWithin {} miles...\nHospital: {}\nAirport: {}\n".format(i[1][0],i[1][1],i[1][2],dist_in_miles,has_Hospital_near,has_airport_near))
-		return output
+# 		# Make it pretty and print that shit. 
+# 		print("City: {}\nState: {}\nPopulation: {}\n\nWithin {} miles...\nHospital: {}\nAirport: {}\n".format(i[1][0],i[1][1],i[1][2],dist_in_miles,has_Hospital_near,has_airport_near))
+# 		return output
 
 
 
